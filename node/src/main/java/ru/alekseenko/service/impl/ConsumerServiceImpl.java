15634bc532a270aa3805ff8f34b4bbce8e708fa4
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-09-19 18:11:29 +0300

    Добавлена интеграция с postgreSQL

--- node/src/main/java/ru/alekseenko/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/ru/alekseenko/service/impl/ConsumerServiceImpl.java
@@ -3,34 +3,26 @@ package ru.alekseenko.service.impl;
 import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.alekseenko.service.ConsumerService;
-import ru.alekseenko.service.ProducerService;
+import ru.alekseenko.service.MainService;
 
 import static ru.alekseenko.RabbitQueue.*;
 
 @Service
 @Log4j
 public class ConsumerServiceImpl implements ConsumerService {
-    private final ProducerService producerService;
+    private final MainService mainService;
 
-    public ConsumerServiceImpl(ProducerService producerService) {
-        this.producerService = producerService;
+    public ConsumerServiceImpl(MainService mainService) {
+        this.mainService = mainService;
     }
+
     @Override
     @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
     public void consumeTextMessageUpdates(Update update) {
         log.debug("NODE: Text message is received");
-
-        Message message = update.getMessage();
-
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId());
-        sendMessage.setText("Hello from NODE");
-
-        producerService.produceAnswer(sendMessage);
+        mainService.processTextMessage(update);
     }
 
     @Override
--- node/src/main/java/ru/alekseenko/service/impl/MainServiceImp.java
+++ node/src/main/java/ru/alekseenko/service/impl/MainServiceImp.java
@@ -1,10 +1,15 @@
 package ru.alekseenko.service.impl;
 
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.alekseenko.dao.RawDataDAO;
+import ru.alekseenko.entity.RawData;
 import ru.alekseenko.service.MainService;
 import ru.alekseenko.service.ProducerService;
 
+@Service
 public class MainServiceImp implements MainService {
 
     private final RawDataDAO rawDataDAO;
@@ -17,6 +22,23 @@ public class MainServiceImp implements MainService {
 
     @Override
     public void processTextMessage(Update update) {
+        saveRawData(update);
 
+        Message message = update.getMessage();
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId());
+        sendMessage.setText("Hello from NODE");
+
+        producerService.produceAnswer(sendMessage);
+
+    }
+
+    private void saveRawData(Update update) {
+        RawData rawData = RawData.builder()
+                .event(update)
+                .build();
+
+        rawDataDAO.save(rawData);
     }
 }
